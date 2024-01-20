250d0079436c147a71aedf61867821a538869214
ansadmk <ansadmk@gmail.com>
2024-01-20 23:18:10 +0530

    ansad.mp4

--- index.ts
+++ index.ts
@@ -24,21 +24,31 @@ app.post('/fbandinsta',async (req:any,res:any)=>{
 //         url:format.url
 //     })
     
-    
-    
-    
 // });
-app.get('/yt360', async (req :any, res:any) => {
+app.get('/yt',async (req:any,res:any)=>{
     const { url } = req.query;
-    const info = await ytdl.getInfo(url);
-    const videoStream = ytdl(url,{ filter: (format:any) => format.qualityLabel === '360p' });
-    const fileStream = fs.createWriteStream(info.videoDetails.videoId+".mp4");
+    const videoId=ytdl.getVideoID(decodeURIComponent(url))
+    const info = await ytdl.getInfo(videoId);
+    console.log(info);
+    
+    res.json({
+        detail:info.videoDetails,
+        formats:info.formats
+    })
+})
+app.get('/ytdown', async (req :any, res:any) => {
+    const { url,quality } = req.query;
+    const videoId=ytdl.getVideoID(decodeURIComponent(url))
+
+    const info = await ytdl.getInfo(videoId);
+    const videoStream = ytdl(decodeURIComponent(url),{ filter: (format:any) => format.qualityLabel === quality });
+    const fileStream = fs.createWriteStream(videoId+".mp4");
     
     videoStream.pipe(fileStream);
 
     fileStream.on('finish', () => {
         // The ytdl and file write operations are complete
-        res.download(info.videoDetails.videoId+".mp4", info.videoDetails.title+".mp4", (err:any) => {
+        res.download(videoId+".mp4", info.videoDetails.title+".mp4", (err:any) => {
             if (err) {
                 console.error(err);
                 res.status(500).json({
@@ -49,123 +59,7 @@ app.get('/yt360', async (req :any, res:any) => {
                 // Optionally, you can remove the file after downloading
                 fs.unlinkSync(info.videoDetails.videoId+".mp4");
                 console.log('File downloaded successfully');
-                res.json({
-                    formats:info.videoDetails.formats
-                })
-            }
-        });
-        
-    });
-
-    fileStream.on('error', (err:any) => {
-        console.error(err);
-        res.status(500).json({
-            status: 'error',
-            error: 'Failed to write the file',
-        });
-    });
-    
-});
-app.get('/yt480', async (req :any, res:any) => {
-    const { url } = req.query;
-    const info = await ytdl.getInfo(url);
-    const videoStream = ytdl(url,{ filter: (format:any) => format.qualityLabel === '480p' });
-    const fileStream = fs.createWriteStream(info.videoDetails.videoId+".mp4");
-    
-    videoStream.pipe(fileStream);
-
-    fileStream.on('finish', () => {
-        // The ytdl and file write operations are complete
-        res.download(info.videoDetails.videoId+".mp4", info.videoDetails.title+".mp4", (err:any) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json({
-                    status: 'error',
-                    error: 'Failed to download the file',
-                });
-            } else {
-                // Optionally, you can remove the file after downloading
-                fs.unlinkSync(info.videoDetails.videoId+".mp4");
-                console.log('File downloaded successfully');
-                res.json({
-                    formats:info.videoDetails.formats
-                })
-            }
-        });
-        
-    });
-
-    fileStream.on('error', (err:any) => {
-        console.error(err);
-        res.status(500).json({
-            status: 'error',
-            error: 'Failed to write the file',
-        });
-    });
-    
-});
-app.get('/yt720', async (req :any, res:any) => {
-    const { url } = req.query;
-    const info = await ytdl.getInfo(url);
-    const videoStream = ytdl(url,{ filter: (format:any) => format.qualityLabel === '720p' });
-    const fileStream = fs.createWriteStream(info.videoDetails.videoId+".mp4");
-    
-    videoStream.pipe(fileStream);
-
-    fileStream.on('finish', () => {
-        // The ytdl and file write operations are complete
-        res.download(info.videoDetails.videoId+".mp4", info.videoDetails.title+".mp4", (err:any) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json({
-                    status: 'error',
-                    error: 'Failed to download the file',
-                });
-            } else {
-                // Optionally, you can remove the file after downloading
-                fs.unlinkSync(info.videoDetails.videoId+".mp4");
-                console.log('File downloaded successfully');
-                res.json({
-                    formats:info.videoDetails.formats
-                })
-            }
-        });
-        
-    });
-
-    fileStream.on('error', (err:any) => {
-        console.error(err);
-        res.status(500).json({
-            status: 'error',
-            error: 'Failed to write the file',
-        });
-    });
-    
-});
-app.get('/yt1080', async (req :any, res:any) => {
-    const { url } = req.query;
-    const info = await ytdl.getInfo(url);
-    const videoStream = ytdl(url,{ filter: (format:any) => format.qualityLabel === '1080p' });
-    const fileStream = fs.createWriteStream(info.videoDetails.videoId+".mp4");
-    
-    videoStream.pipe(fileStream);
-
-    fileStream.on('finish', () => {
-        // The ytdl and file write operations are complete
-        res.download(info.videoDetails.videoId+".mp4", info.videoDetails.title+".mp4", (err:any) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json({
-                    status: 'error',
-                    error: 'Failed to download the file',
-                });
-            } else {
-                // Optionally, you can remove the file after downloading
-                fs.unlinkSync(info.videoDetails.videoId+".mp4");
-                console.log('File downloaded successfully');
-                res.json({
-                    formats:info.videoDetails.formats
-                })
+                
             }
         });
         
