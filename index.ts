e5fc3d780989c5e1ac55bac9c01b4040cdd8d122
ansadmk <ansadmk@gmail.com>
2024-01-21 13:27:14 +0530

    params

--- index.ts
+++ index.ts
@@ -38,10 +38,10 @@ app.get('/yt',async (req:any,res:any)=>{
 })
 app.get('/ytdown', async (req :any, res:any) => {
     const { url,quality } = req.query;
-    const videoId=ytdl.getVideoID(decodeURIComponent(url))
+    const videoId=ytdl.getVideoID(url)
 
     const info = await ytdl.getInfo(videoId);
-    const videoStream = ytdl(decodeURIComponent(url),{ filter: (format:any) => format.qualityLabel === quality });
+    const videoStream = ytdl(url,{ filter: (format:any) => format.qualityLabel === quality });
     const fileStream = fs.createWriteStream(videoId+".mp4");
     
     videoStream.pipe(fileStream);
